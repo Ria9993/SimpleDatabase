48efda8ec4fce11121275409de50efc3e63ec6d8
Hyunjun Kim <hyunjun2372@gmail.com>
2023-06-20 19:53:48 +0900

    Fix error

--- main.cpp
+++ main.cpp
@@ -169,7 +169,7 @@ ExecuteResult execute_insert(Statement& statement, Table& table)
     if (table.num_rows >= TABLE_MAX_ROW)
         return EXECUTE_TABLE_FULL;
     
-    Row* row_to_insert = &(statement->row_to_insert);
+    Row* row_to_insert = &(statement.row_to_insert);
 
     serialize_row(row_to_insert, row_slot(table, table.num_rows));
     table.num_rows += 1;
@@ -183,7 +183,7 @@ ExecuteResult execute_select(Statement& statement, Table& table)
     for (uint32_t i = 0; i < table.num_rows; i++)
     {
         deserialize_row(row_slot(table, i), &row);
-        print_row(&row);
+        print_row(row);
     }
     return EXECUTE_SUCCESS;
 }
